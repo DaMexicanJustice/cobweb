6e7018bcdd4b984b72b5acc49bc65786dd3213b1
Patrick Johansen <mrrichtofen@gmail.com>
2017-06-29 02:16:44 +0200

    Update cobweb.js

--- public_html/assets/js/cobweb.js
+++ public_html/assets/js/cobweb.js
@@ -11,7 +11,7 @@ url = "https://damexicanjustice.github.io/cobweb/public_html/index.html#work"
 window.setInterval(function(){
     $(document).ready(function() {
         $("#launch").click(function() {
-            alert("Loading!");
+          
             $("#cobweb").html(streams);
             $("#chat").html(chats)
             hasLoaded = true;
